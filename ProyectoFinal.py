c7fe0a220433c2f5f401683e5136d9175b47262b
JorgeVera <jorgeaveraf@gmail.com>
2023-11-21 14:54:30 -0600

    x

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -1,15 +1,12 @@
-import random,cv2,numpy as np
+import random
+import cv2
+import numpy as np
 import keyboard
 
-
-
-mapa = np.zeros((20,20), dtype=np.uint8)
-
-nodos = 1 #random.randint(2,10)
-
+mapa = np.zeros((20, 20), dtype=np.uint8)
+nodos = 1
 grafo = {}
 
-#Mapas
 
 def posicionAleatoria(mapa):
     mx, my = mapa.shape
@@ -18,23 +15,16 @@ def posicionAleatoria(mapa):
     mapa[mxa, mya] = 255
     return mxa, mya
 
-def porcentajeNavegable(mapa):
-    porcentaje = random.randint(50,70)
-    return int(porcentaje/100 * mapa.size)
 
 def areaNavegable(mapa):
-    areanavegable = []
-    for i in range(mapa.shape[1]):
-        for j in range(mapa.shape[0]):
-            if mapa[i,j] == 255:
-                areanavegable.append([i,j])
-    return areanavegable
+    porcentaje = random.randint(50, 70)
+    return int(porcentaje / 100 * mapa.size)
 
 
 def caminoAleatorioMapa(mapa, mxa, mya, area):
-    visitadas = []  
-    visitadas.append([mxa,mya])  
-    i = 0 
+    visitadas = []
+    visitadas.append([mxa, mya])
+    i = 0
     while area > 0:
         direccion = random.randint(1, 4)
         dx, dy = 0, 0
@@ -46,65 +36,66 @@ def caminoAleatorioMapa(mapa, mxa, mya, area):
             dx, dy = 0, -1
         elif direccion == 4 and mya < mapa.shape[1] - 1:
             dx, dy = 0, 1
-        
+
         if [mxa + dx, mya + dy] not in visitadas:
-                mxa += dx
-                mya += dy
-                mapa[mxa, mya] = 255
-                visitadas.append([mxa,mya])
-                area -= 1
-                i = 0
+            mxa += dx
+            mya += dy
+            mapa[mxa, mya] = 255
+            visitadas.append([mxa, mya])
+            area -= 1
+            i = 0
         else:
-            i+=1
+            i += 1
             if i >= 10:
                 mxa, mya = posicionAleatoria(mapa)
-                visitadas.append([mxa,mya])
-                area-=1
+                visitadas.append([mxa, mya])
+                area -= 1
                 i = 0
     return mapa
 
-def creacionMapas(nodos,mapa,mx, my, area):
+
+def creacionMapas(nodos, mapa, mx, my, area):
     mapas = {}
     for i in range(nodos):
         mapa = caminoAleatorioMapa(mapa, mx, my, area)
-        mapas.update({i+1:mapa})
-        mapa = np.zeros((mapa.shape[0],mapa.shape[1]), dtype=np.uint8)
+        mapas.update({i + 1: mapa})
+        mapa = np.zeros((mapa.shape[0], mapa.shape[1]), dtype=np.uint8)
     return mapas
 
-#Grafo
 
-def preconexion(grafo,nodos,c):
-    for i in range(1,nodos+1):
+def preconexion(grafo, nodos, c):
+    for i in range(1, nodos + 1):
         grafo[i] = []
         if i != nodos:
-            grafo[i].append(i+1)
-    c-=nodos-1
-    return grafo,nodos,c
+            grafo[i].append(i + 1)
+    c -= nodos - 1
+    return grafo, nodos, c
 
-def conexiones(grafo,nodos,c):
-    grafo,nodos,c = preconexion(grafo,nodos,c)
+
+def conexiones(grafo, nodos, c):
+    grafo, nodos, c = preconexion(grafo, nodos, c)
     while c != 0:
-        randn = random.randint(1,nodos)
-        randc = random.randint(1,nodos)
+        randn = random.randint(1, nodos)
+        randc = random.randint(1, nodos)
         if randc in grafo[randn]:
-            return c,grafo
+            return c, grafo
         else:
             if randn in grafo[randc]:
-                return c,grafo
+                return c, grafo
             else:
                 if randc != randn:
                     if len(grafo[randn]) < 3:
                         grafo[randn].append(randc)
-                        c-=1
+                        c -= 1
                     else:
-                        c = 0 
-    return  grafo
+                        c = 0
+    return grafo
 
-#Juego 
+
 def identificar_salidas(mapa):
     salidas = {}
     nsalidas = 0
-    i=0
+    i = 0
     # Norte
     while i < mapa.shape[1]:
         if mapa[0, i] == 255 and i < mapa.shape[1]:
@@ -112,7 +103,7 @@ def identificar_salidas(mapa):
             nsalidas += 1
             salidas[nsalidas] = []
             while j < mapa.shape[1] and mapa[0, j] == 255:
-                salidas[nsalidas].append([0,j ])
+                salidas[nsalidas].append([0, j])
                 j += 1
             i = j
         else:
@@ -120,12 +111,12 @@ def identificar_salidas(mapa):
     # Este
     i = 0
     while i < mapa.shape[0]:
-        if mapa[ i,mapa.shape[1]-1] == 255 and i < mapa.shape[1]:
+        if mapa[i, mapa.shape[1] - 1] == 255 and i < mapa.shape[1]:
             j = i
             nsalidas += 1
             salidas[nsalidas] = []
-            while j < mapa.shape[1] and mapa[ j,mapa.shape[1]-1] == 255:
-                salidas[nsalidas].append([ j,mapa.shape[1]-1 ])
+            while j < mapa.shape[1] and mapa[i, j] == 255:
+                salidas[nsalidas].append([i, j])
                 j += 1
             i = j
         else:
@@ -138,7 +129,7 @@ def identificar_salidas(mapa):
             nsalidas += 1
             salidas[nsalidas] = []
             while j < mapa.shape[1] and mapa[mapa.shape[0] - 1, j] == 255:
-                salidas[nsalidas].append([mapa.shape[0] - 1,j ])
+                salidas[nsalidas].append([mapa.shape[0] - 1, j])
                 j += 1
             i = j
         else:
@@ -146,17 +137,18 @@ def identificar_salidas(mapa):
     # Oeste
     i = 0
     while i < mapa.shape[0]:
-        if mapa[ i,0] == 255 and i < mapa.shape[1]:
+        if mapa[i, 0] == 255 and i < mapa.shape[1]:
             j = i
             nsalidas += 1
             salidas[nsalidas] = []
-            while j < mapa.shape[1] and mapa[ j,0] == 255:
-                salidas[nsalidas].append([ j,0 ])
+            while j < mapa.shape[1] and mapa[i, j] == 255:
+                salidas[nsalidas].append([i, j])
                 j += 1
             i = j
         else:
             i += 1
     return salidas
+
 
 def jugador(mapa, area_navegable):
     height, width = mapa.shape
@@ -164,7 +156,7 @@ def jugador(mapa, area_navegable):
     moving_image = np.ones((square_size, square_size), dtype=np.uint8) * 127
 
     h, w = moving_image.shape
-    x, y = random.choice(area_navegable)
+    x, y = 300, 300
 
     while True:
         frame = mapa.copy()
@@ -172,19 +164,36 @@ def jugador(mapa, area_navegable):
         y_start, y_end = max(0, y - h // 2), min(height, y + h // 2)
         x_start, x_end = max(0, x - w // 2), min(width, x + w // 2)
 
-        if keyboard.is_pressed('up') and y > h // 2 and [x, y - 1] in area_navegable:
+        if (
+            keyboard.is_pressed("up")
+            and y > h // 2
+            and [x, y - 1] in area_navegable
+        ):
             y -= 1
-        elif keyboard.is_pressed('down') and y < height - h // 2 and [x, y + 1] in area_navegable:
+        elif (
+            keyboard.is_pressed("down")
+            and y < height - h // 2
+            and [x, y + 1] in area_navegable
+        ):
             y += 1
-        elif keyboard.is_pressed('left') and x > w // 2 and [x - 1, y] in area_navegable:
+        elif (
+            keyboard.is_pressed("left")
+            and x > w // 2
+            and [x - 1, y] in area_navegable
+        ):
             x -= 1
-        elif keyboard.is_pressed('right') and x < width - w // 2 and [x + 1, y] in area_navegable:
+        elif (
+            keyboard.is_pressed("right")
+            and x < width - w // 2
+            and [x + 1, y] in area_navegable
+        ):
             x += 1
 
-        frame[y_start:y_end, x_start:x_end] = moving_image[:y_end-y_start, :x_end-x_start]
+        frame[y_start:y_end, x_start:x_end] = moving_image[
+            : y_end - y_start, : x_end - x_start
+        ]
         # Mostrar la ventana
-        cv2.imshow('Mover Cuadrado', frame)
-
+        cv2.imshow("Mover Cuadrado", frame)
 
         # Salir del bucle cuando se presiona la tecla 'esc'
         if cv2.waitKey(1) == 27:
@@ -194,21 +203,27 @@ def jugador(mapa, area_navegable):
     cv2.destroyAllWindows()
 
 
-#principal
+def verificar_coordenadas_en_area(coord, area_navegable):
+    return coord in area_navegable
 
+
+# principal
 mx, my = posicionAleatoria(mapa)
-
-area = porcentajeNavegable(mapa)
-
-mapas = creacionMapas(nodos,mapa,mx, my, area)
 area = areaNavegable(mapa)
+mapas = creacionMapas(nodos, mapa, mx, my, area)
+
+# Obtener área navegable
+area_navegable_coords = [
+    [i, j] for i in range(mapa.shape[0]) for j in range(mapa.shape[1]) if mapa[i, j] == 255
+]
 
 for mapa in mapas.values():
-    mapa = cv2.resize(mapa, (600, 600),interpolation=cv2.INTER_NEAREST)
-    jugador(mapa, area)
-    cv2.imshow('Nueva Imagen',mapa)
+    mapa = cv2.resize(mapa, (600, 600), interpolation=cv2.INTER_NEAREST)
+    jugador(mapa, area_navegable_coords)
+
+    cv2.imshow("Nueva Imagen", mapa)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 salidas = identificar_salidas(mapa)
-print(salidas.keys()) 
+print(salidas.keys())
